fa33da7126df7ba9602bc96bc6eeaba1639e2ac3
Jason Parham <parham@wildme.org>
2019-08-07 22:11:54 -0700

    added better check for iPython

--- utool/util_dbg.py
+++ utool/util_dbg.py
@@ -681,7 +681,7 @@ def inIPython():
     try:
         shell = get_ipython().__class__.__name__
         if shell == 'ZMQInteractiveShell':
-            return True  # Jupyter notebook or qtconsole
+            return True   # Jupyter notebook or qtconsole
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
